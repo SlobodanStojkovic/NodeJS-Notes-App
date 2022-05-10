57062d93990693a4b0e2fa2bf57bf49fb50faf5d
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-10 19:58:15 +0200

    Add info how to import function from other file and call it

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const fs = require("fs");
+//const fs = require("fs");
 
-fs.writeFileSync("notes.txt", "My name is Slobodan Stojkovic.");
+//fs.writeFileSync("notes.txt", "My name is Slobodan Stojkovic.");  //to create a file named notes.txt and put text in it (second argument)
+//fs.appendFileSync("notes.txt", " This is appended text.")   //to append text to a file
+
+const getNotes = require("./notes");
+
+const msg = getNotes();
+
+console.log(msg);
